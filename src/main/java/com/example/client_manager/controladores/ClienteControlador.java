aa8e9151edd6c131f0fea9f560bbc8c987a48016
Diogo Batista <diogobatista330@gmail.com>
2024-08-22 16:12:12 -0300

    fix: metodos do controlador cliente atualizados, bugs corrigidos

--- src/main/java/com/example/client_manager/controladores/ClienteControlador.java
+++ src/main/java/com/example/client_manager/controladores/ClienteControlador.java
@@ -1,11 +1,11 @@
 package com.example.client_manager.controladores;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.client_manager.conversor.ClienteConversor;
 import com.example.client_manager.dto.ClienteDTO;
+import com.example.client_manager.dto.EmailDTO;
+import com.example.client_manager.dto.TelefoneDTO;
 import com.example.client_manager.entidades.Cliente;
-import com.example.client_manager.repositorios.ClienteRepositorio;
+import com.example.client_manager.entidades.Email;
+import com.example.client_manager.entidades.Telefone;
 import com.example.client_manager.servico.ClienteServico;
 
 @RestController
@@ -45,21 +48,58 @@ public class ClienteControlador {
 
 	@PostMapping
 	public ResponseEntity<ClienteDTO> createCliente(@RequestBody ClienteDTO clienteDTO) {
-	    Cliente cliente = ClienteConversor.toEntity(clienteDTO); // Converte DTO para entidade
-	    Cliente novoCliente = clienteServico.save(cliente);
-	    ClienteDTO clienteDTOResponse = ClienteConversor.toDTO(novoCliente); // Converte entidade para DTO
-	    return ResponseEntity.status(HttpStatus.CREATED).body(clienteDTOResponse);
+		Cliente cliente = ClienteConversor.toEntity(clienteDTO); // Converte DTO para entidade
+		Cliente novoCliente = clienteServico.save(cliente);
+		ClienteDTO clienteDTOResponse = ClienteConversor.toDTO(novoCliente); // Converte entidade para DTO
+		return ResponseEntity.status(HttpStatus.CREATED).body(clienteDTOResponse);
 	}
-
 
 	@PutMapping("/{id}")
-	public Cliente updateCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
-		Cliente clienteExistente = clienteServico.findById(id);
-		clienteExistente.setNome(cliente.getNome());
-		clienteExistente.setEndereco(cliente.getEndereco());
+	public ResponseEntity<ClienteDTO> updateCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteDTO) {
+	    Cliente clienteExistente = clienteServico.findById(id);
 
-		return clienteServico.save(clienteExistente);
+	    clienteExistente.setNome(clienteDTO.getNome());
+	    clienteExistente.setEndereco(clienteDTO.getEndereco());
+
+	    clienteExistente.getTelefones().clear();
+	    for (TelefoneDTO telefoneDTO : clienteDTO.getTelefones()) {
+	        Telefone telefone = new Telefone();
+	        telefone.setNumero(telefoneDTO.getNumero());
+	        telefone.setTipo(telefoneDTO.getTipo());
+	        telefone.setCliente(clienteExistente);
+	        clienteExistente.getTelefones().add(telefone);
+	    }
+
+	    // Atualiza emails
+	    List<Email> emailsExistentes = clienteExistente.getEmail();
+	    List<String> novosEmails = clienteDTO.getEmails().stream()
+	    		.map(EmailDTO::getEndereco)
+	    		.collect(Collectors.toList());
+	    
+	    // atualiza ou adiciona novos emails
+	    for (EmailDTO emailDTO : clienteDTO.getEmails()) {
+	    	Email emailExistente = emailsExistentes.stream()
+	    			.filter(email -> email.getEndereco().equals(emailDTO.getEndereco()))
+	    			.findFirst()
+	    			.orElse(null);
+	    	
+	    	if(emailExistente != null) {
+	    		emailExistente.setEndereco(emailDTO.getEndereco());
+	    	} else {
+	    		Email novoEmail = new Email();
+	    		novoEmail.setEndereco(emailDTO.getEndereco());
+	    		novoEmail.setCliente(clienteExistente);
+	    		clienteExistente.getEmail().add(novoEmail);
+	    	}
+	    }
+	    
+	    clienteExistente.getEmail().removeIf(email -> !novosEmails.contains(email.getEndereco()));
+	    
+	    Cliente clienteAtualizado = clienteServico.save(clienteExistente);
+	    ClienteDTO clienteDTOResponse = ClienteConversor.toDTO(clienteAtualizado);
+	    return ResponseEntity.ok(clienteDTOResponse);
 	}
+
 
 	@DeleteMapping("/{id}")
 	public void deleteCliente(@PathVariable Long id) {
